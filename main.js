8640ba2abdd3ababac0379684d229bf3f667d496
Jimwell Ocsio <arvinjimwell@gmail.com>
2023-03-22 00:13:17 +0800

    add a mouseover parody

--- main.js
+++ main.js
@@ -7,7 +7,24 @@ createApp({
             description: "This socks to be wear!",
             image: "./stress.jpg",
             stress_level: 100,
-            cars: ["BMW", "Toyota", "Mitsubishi", "BMX"]
+            cars: ["BMW", "Toyota", "Mitsubishi", "BMX"],
+            count: 0
+        }
+    },
+    methods: {
+        changeImage() {
+            if(this.image == "./stress.jpg"){
+                this.image = "./woah.jpg"
+                this.stress_level = 20;
+                this.description = this.count > 1 ? "Oops you found me again!":"Oops you found me!"
+            }
+            else
+            {
+                this.image = "./stress.jpg"
+                this.stress_level = 100
+                this.description = this.count > 10 ? "Stop hovering me" : "Stress again"
+            }
+            this.count++
         }
     }
 }).mount('#app')
